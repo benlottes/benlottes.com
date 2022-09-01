d2055d880db340d5acee5857f1477389e3f6b8e3
Ben Lottes <benclottes@gmail.com>
2022-09-01 15:39:19 -0500

    Added placeholder profile picture and installed bootstrap for styling

--- benlottes-react-app/src/my_components/NamePlate.js
+++ benlottes-react-app/src/my_components/NamePlate.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
 
 class NamePlate extends React.Component{
     render(){
         return (
             <div name="Bio">
-                <h1 name="my_name">Ben Lottes</h1>
+                <div name="name_photo" className="name-plate">
+                    <Image src={require("./../photos/placeholder.jpg")} className="img-thumbnail" style={{width : 75, height: 75, borderRadius: 100/2}} />
+                    <h1 name="my_name">Ben Lottes</h1>
+                </div>
                 <a name="github_link" href="https://github.com/benlottes" target="_blank" rel="noreferrer noopener">My github</a>
                 <p name="email">benclottes@gmail.com</p>
             </div>
