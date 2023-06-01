1828f89f549b8ccffa29b3c464e7d15485a76e2a
Ben Lottes <benclottes@gmail.com>
2023-06-01 13:08:49 -0500

    Updated home page bio

--- benlottes-react-app/src/my_components/Home.js
+++ benlottes-react-app/src/my_components/Home.js
@@ -11,16 +11,14 @@ class Home extends React.Component{
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', maxWidth: '600px'}}>
                             <h1><u>Hello!</u></h1>
                             <p style={{ textAlign: 'justify' }}>
-                               My name is Ben Lottes, I am a student at The University of Kansas
-                               pursuing a degree in Computer Science and a certificate in cyber
-                               security. I am due to graduate in May, and I am very excited
-                               to start my career in software development! I'm a hard worker,
-                               excel at problem solving and I hope to land a job where I can continue 
-                               to learn and grow as a developer. I find myself much more confident 
-                               working on the logic in the backend of applications; nothing feels 
-                               better than finding an elegant solution to a tough problem. I am 
-                               currently applying for full-time positions starting after May 2023.
-                               Please feel free to reach out to me at my email address above!
+                               My name is Ben Lottes, I recently graduated from The University of Kansas 
+                               after completing a Bachelor's degree in computer science and a certificate 
+							   in cyber security. I'm a hard worker, excel at problem solving and I 
+							   hope to land a job where I can continue to learn and grow as a developer. 
+							   I find myself much more confident working on the logic in the backend of 
+							   applications; nothing feels better than finding an elegant solution to a 
+							   tough problem. I am currently applying for full-time positions, so please
+							   feel free to reach out to me at my email address above!
                             </p>
                         </div>                
                     </div>
